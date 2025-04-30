19f087ea642265ef0d809a5ffe06541d6e8bf5e2
Joao Emanuel <joaoemanuel20181@outlook.com>
2025-04-30 07:18:32 -0300

    Endpoint / pedido/id

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Entity;
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 
--- src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
@@ -3,6 +3,7 @@ package com.nelioalves.cursomc.domain;
 import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
 import jakarta.persistence.Entity;
@@ -22,6 +23,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 
+	@JsonManagedReference
 	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
